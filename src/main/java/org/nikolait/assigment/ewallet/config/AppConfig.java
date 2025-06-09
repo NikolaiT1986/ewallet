bf62edfdf0b0983666e4028cc2c41a4f62a4dcf5
NikolaiT1986 <kolyan-04031986@mail.ru>
2025-06-09 19:56:42 +0300

    feature. add jdbc connection timeout properties

--- src/main/java/org/nikolait/assigment/ewallet/config/AppConfig.java
+++ src/main/java/org/nikolait/assigment/ewallet/config/AppConfig.java
@@ -3,9 +3,11 @@ package org.nikolait.assigment.ewallet.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
+import javax.sql.DataSource;
 import java.util.Locale;
 
 @Configuration
@@ -14,9 +16,19 @@ public class AppConfig {
     @Value("${app.locale:en}")
     private String fixedLocaleTag;
 
+    @Value("${app.jdbc.query-timeout-seconds:1}")
+    private int queryTimeoutSeconds;
+
     @Bean
     public LocaleResolver localeResolver() {
         return new FixedLocaleResolver(Locale.forLanguageTag(fixedLocaleTag));
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.setQueryTimeout(queryTimeoutSeconds);
+        return jdbcTemplate;
     }
 
 }
